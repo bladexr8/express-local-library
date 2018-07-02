38886176c4751fedb613ab3b80e994975b067d5c
Stephen Moss <bladexr8@gmail.com>
2018-07-02 22:12:11 +1000

    Completed Part 4

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
